b19d44b5eb246329b995ef9178acff0bafcfd9fe
Maria Clara <cllarabarreto20@sempreceub.com>
2025-05-10 21:01:19 -0300

    Adicionar exemplo senha de indentificaçao

---
+++ exemplo para o usuaria abrir uma porta.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+int senha_escritorio_do_presidente =2448 ;
+
+(senha_escritorio_do_presidente == 2448 )? printf("senha correta, acesso liberado")
+
+
+    : printf("senha incorreta, acesso negado");
+
+    return 0;
+}

7e6884fcbd594b491a33cc9e77d302a85b09198e
Maria Clara <cllarabarreto20@sempreceub.com>
2025-05-11 00:10:24 -0300

    Adicionar ex cauculo

---
+++ Calcular a área de um retângulo.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+
+//criacao de variaveis
+int largura = 4;
+int altura = 6;
+int area;
+
+//caucular area
+area = largura * altura;
+
+// imprimir as variaveis
+
+printf("largura e: %d\n",largura);
+printf ("altura e: %d\n",altura);
+printf ("a area do retangulo e: %d\n",area);
+
+return 0;
+
+
+
+
+}
